9bb83828e0763b7be227a56ee5bec34c3cc8bb05
Scoops Martin <michael.martin@cleancoders.com>
2024-07-24 11:23:24 -0500

    Updated main to target main instead of core.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main tic-tac-toe.core
+  :main main
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[speclj "3.4.6"]]}}
   :plugins [[speclj "3.4.6"]]
