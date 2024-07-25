8a41da4825cb5865599033f79f16bff43fe1da1e
Scoops Martin <michael.martin@cleancoders.com>
2024-07-25 11:32:07 -0500

    moved take-win and take-block to move-core.

--- spec/tic_tac_toe/moves/medium_spec.clj
+++ spec/tic_tac_toe/moves/medium_spec.clj
@@ -10,21 +10,4 @@
     (it "returns index of blocking move if winning move isn't available"
       (should= 2 (sut/get-medium-move ["X" "X" 2 "O" 4 5 "O" 7 "X"])))
     )
-
-  (context "take-block"
-    (it "tests"
-      (should= 2 (sut/take-block "O" ["X" "X" 2 "O" 4 5 "O" 7 "X"]))))
-
-  (context "win-next-turn?"
-    (it "returns true for win from x in next turn"
-      (should (sut/win-next-turn? "X" ["X" "X" 2 "O" "O" 5 6 7 8])))
-
-    (it "returns true for win from o in next turn"
-      (should (sut/win-next-turn? "O" ["X" 1 2 "O" "O" 5 "X" 7 8])))
-    )
-
-  (context "take-block"
-    (it "returns index of blocking move"
-      (should= 2 (sut/take-block "X" ["O" "O" 2 "X" "X" 5 6 7 8]))))
-
   )
--- src/tic_tac_toe/moves/medium.clj
+++ src/tic_tac_toe/moves/medium.clj
@@ -1,28 +1,12 @@
 (ns tic-tac-toe.moves.medium
-  (:require [tic-tac-toe.board :as board]
-            [tic-tac-toe.core :as core]))
-
-(defn take-win
-  ([player board] (take-win player board (core/get-available-moves board)))
-
-  ([player board moves]
-   (if (or (empty? moves) (board/win? player (core/update-board player (first moves) board)))
-     (first moves)
-     (recur player board (rest moves)))))
-
-(defn take-block
-  ([player board] (take-win (core/switch-player player) board (core/get-available-moves board))))
-
-(defn win-next-turn? [player board]
-  (let [moves (core/get-available-moves board)
-        next-move-wins (filter #(board/win? player (core/update-board player % board)) moves)]
-    (not (empty? next-move-wins))))
+  (:require [tic-tac-toe.core :as core]
+            [tic-tac-toe.moves.core :as moves-core]))
 
 (defn- get-medium-move [board]
   (let [active-player (core/find-active-player board)]
     (cond
-      (win-next-turn? active-player board) (take-win active-player board)
-      (win-next-turn? (core/switch-player active-player) board) (take-block active-player board)
+      (moves-core/win-next-turn? active-player board) (moves-core/take-win active-player board)
+      (moves-core/win-next-turn? (core/switch-player active-player) board) (moves-core/take-block active-player board)
       :else (first (core/get-available-moves board)))))
 
 (def get-medium-move (memoize get-medium-move))
