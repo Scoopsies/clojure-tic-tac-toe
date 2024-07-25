422fa3823a6706280b8a18c52b0d04aef63b9377
Scoops Martin <michael.martin@cleancoders.com>
2024-07-25 17:25:13 -0500

    Created new behavior for 3x3x3.

--- src/tic_tac_toe/moves/medium.clj
+++ src/tic_tac_toe/moves/medium.clj
@@ -2,12 +2,23 @@
   (:require [tic-tac-toe.core :as core]
             [tic-tac-toe.moves.core :as moves-core]))
 
-(defn- get-medium-move [board]
-  (let [active-player (core/find-active-player board)]
+(defmulti get-medium-move count)
+
+(defmethod get-medium-move :default [board]
+  (let [active-player (core/find-active-player board)
+        available-moves (core/get-available-moves board)]
     (cond
       (moves-core/win-next-turn? active-player board) (moves-core/take-win active-player board)
       (moves-core/lose-next-turn? active-player board) (moves-core/take-block active-player board)
-      :else (first (core/get-available-moves board)))))
+      :else (first available-moves))))
+
+(defmethod get-medium-move 27 [board]
+  (let [active-player (core/find-active-player board)
+        available-moves (core/get-available-moves board)]
+    (cond
+      (moves-core/lose-next-turn? active-player board) (moves-core/take-block active-player board)
+      (some #{13} board) 13
+      :else (rand-nth available-moves))))
 
 (def get-medium-move (memoize get-medium-move))
 
