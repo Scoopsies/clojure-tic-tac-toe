6cc74ee65d9fab4c656a60681c09872363da7adf
Scoops Martin <michael.martin@cleancoders.com>
2024-07-25 11:31:47 -0500

    Added testing for 3x3x3 to check it still works.

--- spec/tic_tac_toe/moves/core_spec.clj
+++ spec/tic_tac_toe/moves/core_spec.clj
@@ -3,6 +3,12 @@
             [tic-tac-toe.moves.core :as sut]
             [tic-tac-toe.spec-helper :as helper]))
 
+(defn map-add-nine [coll]
+  (map #(+ 9 %) coll))
+
+(defn map-add-18 [coll]
+  (map #(+ 18 %) coll))
+
 (describe "moves-core"
   (context "win-next-turn?"
     (it "returns true for win from x in next turn"
@@ -68,6 +74,64 @@
           (test 13 [[1 5 9] [12 14 15]])
           (test 14 [[2 6 10] [12 13 15]])
           (test 15 [[0 5 10] [3 7 11] [12 13 14]])))
+      )
+
+    (context "3x3x3"
+      (context "facing forwards"
+        (it "takes any presented win on plane 0"
+          (let [board (range 27)
+                test (fn [result played-spots]
+                       (doseq [x played-spots]
+                         (should= result (sut/take-win "X" (helper/populate-board "X" x board)))))]
+
+            (test 0 [[1 2] [4 8] [3 6]])
+            (test 1 [[0 2] [4 7]])
+            (test 2 [[0 1] [4 6] [5 8]])
+
+            (test 3 [[0 6] [4 5]])
+            (test 4 [[0 8] [1 7] [2 6] [3 5]])
+            (test 5 [[2 8] [3 4]])
+
+            (test 6 [[0 3] [2 4] [7 8]])
+            (test 7 [[1 4] [6 8]])
+            (test 8 [[0 4] [2 5] [6 7]])))
+
+        (it "takes any presented win on plane 1"
+          (let [board (range 27)
+                test (fn [result played-spots]
+                       (doseq [x played-spots]
+                         (should= result (sut/take-win "X" (helper/populate-board "X" x board)))))]
+
+            (test (+ 9 0) (map map-add-nine [[1 2] [4 8] [3 6]]))
+            (test (+ 9 1) (map map-add-nine [[0 2] [4 7]]))
+            (test (+ 9 2) (map map-add-nine [[0 1] [4 6] [5 8]]))
+
+            (test (+ 9 3) (map map-add-nine [[0 6] [4 5]]))
+            (test (+ 9 4) (map map-add-nine [[0 8] [1 7] [2 6] [3 5]]))
+            (test (+ 9 5) (map map-add-nine [[2 8] [3 4]]))
+
+            (test (+ 9 6) (map map-add-nine [[0 3] [2 4] [7 8]]))
+            (test (+ 9 7) (map map-add-nine [[1 4] [6 8]]))
+            (test (+ 9 8) (map map-add-nine [[0 4] [2 5] [6 7]]))))
+
+        (it "takes any presented win on plane 2"
+          (let [board (range 27)
+                test (fn [result played-spots]
+                       (doseq [x played-spots]
+                         (should= result (sut/take-win "X" (helper/populate-board "X" x board)))))]
+
+            (test (+ 18 0) (map map-add-18 [[1 2] [4 8] [3 6]]))
+            (test (+ 18 1) (map map-add-18 [[0 2] [4 7]]))
+            (test (+ 18 2) (map map-add-18 [[0 1] [4 6] [5 8]]))
+
+            (test (+ 18 3) (map map-add-18 [[0 6] [4 5]]))
+            (test (+ 18 4) (map map-add-18 [[0 8] [1 7] [2 6] [3 5]]))
+            (test (+ 18 5) (map map-add-18 [[2 8] [3 4]]))
+
+            (test (+ 18 6) (map map-add-18 [[0 3] [2 4] [7 8]]))
+            (test (+ 18 7) (map map-add-18 [[1 4] [6 8]]))
+            (test (+ 18 8) (map map-add-18 [[0 4] [2 5] [6 7]]))))
+        )
       )
     )
   )
