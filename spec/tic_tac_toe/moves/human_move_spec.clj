92e5713014559689bff09439c291c9416d151bd4
Scoops Martin <michael.martin@cleancoders.com>
2024-07-25 20:48:51 -0500

    "Moved test to printables that tested the printing of the function."

--- spec/tic_tac_toe/moves/human_move_spec.clj
+++ spec/tic_tac_toe/moves/human_move_spec.clj
@@ -15,13 +15,6 @@
           (sut/get-human-move (range 0 9))
           (with-in-str "33\n3")
           (should= 2))))
-
-    (it "tells player to enter a valid move if move isn't available"
-      (should= "3 is not a valid move\nPlease enter a valid move.\n"
-               (->>
-                 (sut/get-human-move [0 1 "X" 3])
-                 (with-in-str "3\n4")
-                 (with-out-str))))
     )
 
   (context "update-board-human"
