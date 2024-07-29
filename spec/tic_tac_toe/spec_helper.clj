dfc643477dd51719fe356bf96096b7f17d637575
Scoops Martin <michael.martin@cleancoders.com>
2024-07-29 11:09:30 -0500

    Added a way to populate board with both player tokens.

--- spec/tic_tac_toe/spec_helper.clj
+++ spec/tic_tac_toe/spec_helper.clj
@@ -2,5 +2,11 @@
   (:require [speclj.core :refer :all]
             [tic-tac-toe.core :as core]))
 
-(defn populate-board [player-token positions board]
-  (reduce #(core/update-board player-token %2 %1) board positions))
+(defn populate-board
+  ([player-token positions board]
+   (reduce #(core/update-board player-token %2 %1) board positions))
+
+  ([player-token positions-1 positions-2 board]
+   (let [board (populate-board player-token positions-1 board)
+         player-token (core/switch-player player-token)]
+     (populate-board player-token positions-2 board))))
