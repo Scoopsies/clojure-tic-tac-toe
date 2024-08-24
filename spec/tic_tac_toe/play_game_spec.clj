249f0159faa909f140d187cbf92638db233326a5
Scoops Martin <michael.martin@cleancoders.com>
2024-08-23 19:36:39 -0500

    Updated tests.

--- spec/tic_tac_toe/play_game_spec.clj
+++ spec/tic_tac_toe/play_game_spec.clj
@@ -6,10 +6,10 @@
             [tic-tac-toe.play-game :as sut]
             [tic-tac-toe.printables :as printables]))
 
-(def one-game-X "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  O  |  8  |  X  \n\nO wins!\nWould you like to play again? Y/N\nSee you next time!\n")
-(def two-games-X  "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  O  |  8  |  X  \n\nO wins!\nWould you like to play again? Y/N\nWho will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  O  |  8  |  X  \n\nO wins!\nWould you like to play again? Y/N\nSee you next time!\n")
-(def one-game-O "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nX wins!\nWould you like to play again? Y/N\nSee you next time!\n")
-(def two-games-O "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nX wins!\nWould you like to play again? Y/N\nWho will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  X  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nX wins!\nWould you like to play again? Y/N\nSee you next time!\n")
+(def one-game-X "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\nO wins!\n\nPlay Again?\n1. Yes\n2. No\n\nSee you next time!\n\n")
+(def two-games-X  "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\nO wins!\n\nPlay Again?\n1. Yes\n2. No\n\nWho will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  2  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  3  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  X  |  X  |  O  \n  4  |  O  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\nO wins!\n\nPlay Again?\n1. Yes\n2. No\n\nSee you next time!\n\n")
+(def one-game-O "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\nX wins!\n\nPlay Again?\n1. Yes\n2. No\n\nSee you next time!\n\n")
+(def two-games-O "Who will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\nX wins!\n\nPlay Again?\n1. Yes\n2. No\n\nWho will play as X?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWho will play as O?\n1. Human\n2. Computer Easy\n3. Computer Medium\n4. Computer Hard\n\nWhat size board?\n1. 3x3\n2. 4x4\n3. 3x3x3 (3-D)\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  9  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  3  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  8  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  7  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  5  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  2  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\n  1  |  O  |  O  \n  4  |  X  |  6  \n  O  |  X  |  X  \n\nPlease pick a number 1-9.\n\nX wins!\n\nPlay Again?\n1. Yes\n2. No\n\nSee you next time!\n\n")
 (def board-size-menu (printables/get-board-size-menu {:ui :tui}))
 
 (describe "play-game"
@@ -17,30 +17,6 @@
 
   (redefs-around [config/data-store :memory])
   (before (data/write-db []))
-
-  (context "play-again?"
-    (it "returns true if user inputs yes variations"
-      (with-redefs [println (stub :println)]
-        (should (with-in-str "Yes" (sut/play-again?)))
-        (should (with-in-str "YES" (sut/play-again?)))
-        (should (with-in-str "yes" (sut/play-again?)))
-        (should (with-in-str "y" (sut/play-again?)))
-        (should (with-in-str "Y" (sut/play-again?)))))
-
-    (it "returns false if user inputs \"no\" variations"
-      (with-redefs [println (stub :println)]
-        (should-not (with-in-str "NO" (sut/play-again?)))
-        (should-not (with-in-str "No" (sut/play-again?)))
-        (should-not (with-in-str "no" (sut/play-again?)))
-        (should-not (with-in-str "n" (sut/play-again?)))
-        (should-not (with-in-str "N" (sut/play-again?))))
-      )
-
-    (it "repeats question until user responds with valid input."
-      (should= (apply str (repeat 2 "Would you like to play again? Y/N\n"))
-               (with-out-str
-                 (with-in-str "lol\nn" (sut/play-again?)))))
-    )
 
   (context "handle-replay"
     (it "returns the state of the last game if selection is 1"
@@ -112,7 +88,6 @@
                 :board-size :3x3
                 :board (range 9)
                 :ui :tui
-                :menu? false
                 :printables (printables/get-move-printables (range 9))}
                (sut/get-next-state {"X" {:move :hard} "O" {:move :hard} :ui :tui} "1")))
 
@@ -121,7 +96,6 @@
                 "O" {:move :hard}
                 :board-size :4x4
                 :board (range 16)
-                :menu? false
                 :ui :tui
                 :printables (printables/get-move-printables (range 16))}
                (sut/get-next-state {"X" {:move :hard} "O" {:move :hard} :ui :tui} "2")))
@@ -131,7 +105,6 @@
                 "O" {:move :hard}
                 :board-size :3x3x3
                 :board (range 27)
-                :menu? false
                 :ui :tui
                 :printables (printables/get-move-printables (range 27))}
                (sut/get-next-state {"X" {:move :hard} "O" {:move :hard} :ui :tui} "3")))
@@ -220,20 +193,20 @@
       (redefs-around [sut/loop-game-play (stub :loop)])
       (it "assigns previous game printables if last-game unfinished"
         (let [last-game {:id 1 :board ["X" 1 2 3 4 5 6 7 8]}
-              result-state {:ui :tui :printables printables/continue-printables :last-game last-game}]
+              result-state {:ui :tui :printables printables/continue-printables :last-game last-game :game-over? false}]
           (data/write-db [last-game])
           (sut/start-game {:ui :tui})
           (should-have-invoked :loop {:with [result-state]})))
 
       (it "assigns player-x printables if last-game was finished"
         (let [last-game {:id 1 :board ["X" "X" "X" 3 4 5 6 7 8]}
-              result-state {:ui :tui :printables printables/player-x-printables}]
+              result-state {:ui :tui :printables printables/player-x-printables :game-over? false}]
           (data/write-db [last-game])
           (sut/start-game {:ui :tui})
           (should-have-invoked :loop {:with [result-state]})))
 
       (it "assigns player-x printables if no last game"
-        (let [result-state {:ui :tui :printables printables/player-x-printables}]
+        (let [result-state {:ui :tui :printables printables/player-x-printables :game-over? false}]
           (sut/start-game {:ui :tui})
           (should-have-invoked :loop {:with [result-state]})))
       )
@@ -241,22 +214,22 @@
     (context "plays-games"
       (it "plays one game with human as X"
         (should= one-game-X (->> (sut/start-game {:ui :tui})
-                                 (with-in-str "1\n4\n1\n1\n2\n9\nNO")
+                                 (with-in-str "1\n4\n1\n1\n2\n9\n2")
                                  (with-out-str))))
 
       (it "plays two games with human as X"
         (should= two-games-X (->> (sut/start-game {:ui :tui})
-                                  (with-in-str "1\n4\n1\n1\n2\n9\nYES\n1\n4\n1\n1\n2\n9\nNO")
+                                  (with-in-str "1\n4\n1\n1\n2\n9\n1\n1\n4\n1\n1\n2\n9\n2")
                                   (with-out-str))))
 
       (it "plays one game with human as O"
         (should= one-game-O (->> (sut/start-game {:ui :tui})
-                                 (with-in-str "4\n1\n1\n3\n7\n2\nN")
+                                 (with-in-str "4\n1\n1\n3\n7\n2\n2")
                                  (with-out-str))))
 
       (it "plays two games with human as O"
         (should= two-games-O (->> (sut/start-game {:ui :tui})
-                                  (with-in-str "4\n1\n1\n3\n7\n2\nYES\n4\n1\n1\n3\n7\n2\nN")
+                                  (with-in-str "4\n1\n1\n3\n7\n2\n1\n4\n1\n1\n3\n7\n2\n2")
                                   (with-out-str)))))
     )
   )
