458d77e8ae49bc339b0a55534dd36a9b44ad878a
Scoops Martin <michael.martin@cleancoders.com>
2024-08-27 14:10:27 -0500

    moved getting ui to state initializer.

--- src/main.clj
+++ src/main.clj
@@ -2,12 +2,5 @@
   (:require [tic-tac-toe.play-game :as game]
             [tic-tac-toe.gui]))
 
-(defn print-menu []
-  (doseq [menu ["Would you like to play in the terminal or the gui?"
-                "1. Terminal"
-                "2. Gui"
-                "3. Exit"]]
-          (println menu)))
-
 (defn -main [& args]
   (game/start-game args))
